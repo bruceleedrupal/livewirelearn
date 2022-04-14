4a493c7efbdfa497c62514799fca8bbaa55eb606
brucelee <brucelee.drupal@gmail.com>
2022-04-14 17:28:18 +0800

    save

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
+import { initial } from "lodash";
 
 window.Alpine = Alpine;
 
+Alpine.plugin(collapse);
+Alpine.directive("uppercase", (el) => {
+    el.textContent = el.textContent.toUpperCase();
+});
+
 Alpine.start();
+
+console.log(ClassicEditor);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,43 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div x-data="{ open: false }">
+        <button x-on:click="open = ! open">Toggle Dropdown</button>
+
+        <div x-cloak x-show="open">
+            Dropdown Contents...
+        </div>
+    </div>
+
+    <div class="py-12" id="test">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
                 <x-jet-welcome />
+
+                <div x-data="{ expanded: false }">
+                    <button @click="expanded = ! expanded">Toggle Content</button>
+
+                    <p x-show="expanded" x-collapse.min.50px x-uppercase>
+                        Reprehenderit eu excepteur ullamco esse cillum reprehenderit exercitation labore non. Dolore
+                        dolore ea dolore veniam sint in sint ex Lorem ipsum. Sint laborum deserunt deserunt amet
+                        voluptate cillum deserunt. Amet nisi pariatur sit ut id. Ipsum est minim est commodo id dolor
+                        sint id quis sint Lorem.
+                        Reprehenderit eu excepteur ullamco esse cillum reprehenderit exercitation labore non. Dolore
+                        dolore ea dolore veniam sint in sint ex Lorem ipsum. Sint laborum deserunt deserunt amet
+                        voluptate cillum deserunt. Amet nisi pariatur sit ut id. Ipsum est minim est commodo id dolor
+                        sint id quis sint Lorem.
+                        Reprehenderit eu excepteur ullamco esse cillum reprehenderit exercitation labore non. Dolore
+                        dolore ea dolore veniam sint in sint ex Lorem ipsum. Sint laborum deserunt deserunt amet
+                        voluptate cillum deserunt. Amet nisi pariatur sit ut id. Ipsum est minim est commodo id dolor
+                        sint id quis sint Lorem.
+                        Reprehenderit eu excepteur ullamco esse cillum reprehenderit exercitation labore non. Dolore
+                        dolore ea dolore veniam sint in sint ex Lorem ipsum. Sint laborum deserunt deserunt amet
+                        voluptate cillum deserunt. Amet nisi pariatur sit ut id. Ipsum est minim est commodo id dolor
+                        sint id quis sint Lorem.
+                    </p>
+                </div>
+
             </div>
         </div>
     </div>
