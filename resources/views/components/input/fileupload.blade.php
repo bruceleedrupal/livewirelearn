4a883f1da0ce3792f39d6a6b8017d3d0a111d845
brucelee <brucelee.drupal@gmail.com>
2022-05-22 18:50:50 +0800

    save

--- resources/views/components/input/fileupload.blade.php
+++ resources/views/components/input/fileupload.blade.php
@@ -5,6 +5,12 @@
     <script src="/asset/filepond/filepond-plugin-file-validate-size.js"></script>   
     <script src="/asset/filepond/filepond-plugin-file-validate-type.js"></script>  
     <script src="/asset/filepond/zh-cn.js"></script>  
+    <script>
+      FilePond.registerPlugin(FilePondPluginImagePreview)
+      FilePond.registerPlugin(FilePondPluginFileValidateSize)
+      FilePond.registerPlugin(FilePondPluginFileValidateType);
+      FilePond.setOptions(filepond_zh_CN);
+    </script>
 @endPushOnce
 
 @pushOnce('styles')
@@ -12,30 +18,20 @@
   <link href="/asset/filepond/filepond-plugin-image-preview.css" rel="stylesheet" />      
 @endPushOnce
 
-<div x-data="{ count: 0 }" x-init="count++">
-    <span x-text="count"></span>
-    <button @click="count++">Add</button>
-</div>
+
 <div
-  x-data
+  x-data="{Pond:null}"
   x-on:remove-images="Pond.removeFiles()"
   x-init="
-    FilePond.registerPlugin(FilePondPluginImagePreview)
-    FilePond.registerPlugin(FilePondPluginFileValidateSize)
-    FilePond.registerPlugin(FilePondPluginFileValidateType);
-    FilePond.setOptions(filepond_zh_CN);
-
-    FilePond.setOptions({
+    Pond = FilePond.create($refs.input);
+    Pond.setOptions({
         acceptedFileTypes: @if(is_array($filetypes)) {{ json_encode($filetypes, true) }} @else ['{{ $filetypes }}'] @endif,
         imagePreviewHeight: 100,
         maxFileSize: 'Number({{ max_upload_filesize() }}) * 1000',
         allowMultiple: {{ isset($attributes['multiple']) ? 'true' : 'false' }},        
         onprocessfile: (error, file) => {
-          if (!error) {
-            console.log(Pond.getFiles());
-            Pond.removeFile(file.id)
-            console.log(file.id)
-            
+          if (!error) {            
+            Pond.removeFile(file.id);
           }
         },
         server: {
@@ -47,10 +43,8 @@
           },
       },
 
-    });
-    if(Pond is defined) console.log(Pond);
-    Pond = FilePond.create($refs.input);
-
+    });  
+ 
     this.addEventListener('pondReset', e => {
         Pond.removeFiles();
     });
